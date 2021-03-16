6ad9fd5d8110f41dbe3937d8da28d04f4d7fd348
MidnightKittenCat <66114416+MidnightKittenCat@users.noreply.github.com>
2021-03-16 23:17:25 +1000

    Update Project XL.lua

--- Project XL.lua
+++ Project XL.lua
@@ -1,1 +1,17 @@
-print(identityexecutor())
+print(getexecutorname())
+
+if identifyexecutor and identifyexecutor() == "Fluxus" then
+print("Fluxus")
+end
+
+if identifyexecutor and identifyexecutor() == "PRXL" then
+print("PRXL")
+end
+
+if identifyexecutor and identifyexecutor() == "Prxl" then
+print("Prxl")
+end
+
+if identifyexecutor and identifyexecutor() == "prxl" then
+  print("prxl")
+end
